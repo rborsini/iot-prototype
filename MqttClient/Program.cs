a94a4c4530aa5016a2ce96f05d4cf1c80297788f
Roberto Borsini <roberto.borsini@gmail.com>
2020-12-29 07:53:58 +0100

    Add project files.

---
+++ MqttClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MqttClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new MqttFactory();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId("Client1")
+                .WithTcpServer("localhost")
+                .Build();
+
+            var mqttClient = factory.CreateMqttClient();
+
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+
+
+            });
+
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                // Subscribe to a topic
+                await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("mqttdemo/setpoint").Build());
+
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+            Console.WriteLine("connected");
+
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("mqttdemo/setpoint")
+                .WithPayload("1")
+                //.WithExactlyOnceQoS()docke
+                //.WithRetainFlag()
+                .Build();
+
+            await mqttClient.PublishAsync(message, CancellationToken.None);
+
+            Console.WriteLine("published");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MqttServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using MQTTnet;
+using MQTTnet.Server;
+using System;
+
+namespace MqttServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            mqttServer.StartAsync(new MqttServerOptions());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+            mqttServer.StopAsync();
+
+        }
+    }
+}
