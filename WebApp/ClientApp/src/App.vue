f7ca6cd549c8640e5da328ee1eee3b0480abb715
Roberto Borsini <roberto.borsini@gmail.com>
2021-01-16 09:52:54 +0100

    upgrade client app to vue 2 class component

--- WebApp/ClientApp/src/App.vue
+++ WebApp/ClientApp/src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <nav-menu></nav-menu>
-  <router-view />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-    import NavMenu from './components/NavMenu.vue'
-
-export default {
-  name: 'App',
-  components: {
-      NavMenu
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
